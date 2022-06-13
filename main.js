93410ac121a85bc3cc14879b7296424e342732c6
Jeelz <56708384+Jeelz@users.noreply.github.com>
2022-06-13 15:12:26 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+var input = 'Dont let your dreams be memes';
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+var resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+    //console.log(i)
+
+    for (let j = 0; j < vowels.length; j++) {
+        //console.log(j)
+
+        if (input[i] === vowels[j]) {
+            resultArray.push(input[i]);
+}
+    }
+
+    if(input[i] === 'e'){
+    resultArray.push(input[i]);
+}
+
+if(input[i] === 'u'){
+    resultArray.push(input[i]);
+}
+}
+console.log(resultArray.join('').toUpperCase());
